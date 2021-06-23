ecb73dbb63e499e06ed420535583deb93fc5792c
cyy <cyyever@outlook.com>
2021-06-23 23:47:29 +0800

    add test

--- fuzz_test/encode_png_test.cpp
+++ fuzz_test/encode_png_test.cpp
@@ -16,10 +16,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     }
     auto data = torch::empty({static_cast<long>(Size)}, torch::kU8);
     memcpy(data.data_ptr<uint8_t>(), Data, Size);
-    auto img =
-        vision::image::decode_image(data, vision::image::IMAGE_READ_MODE_RGB);
-    /* auto v2=vision::image::encode_png(img,9); */
-    /* vision::image::write_file("/home/cyy/2023.png",v2); */
+    auto image = vision::image::encode_png(data, 5);
 
   } catch (...) {
   }
