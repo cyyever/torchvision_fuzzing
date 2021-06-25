598fbd1669993a3a3d6452005413787c769c35e4
cyy <cyyever@outlook.com>
2021-06-25 16:12:26 +0800

    fix bug

--- fuzz_test/encode_jpeg_test.cpp
+++ fuzz_test/encode_jpeg_test.cpp
@@ -17,9 +17,9 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     std::vector<torch::ScalarType> dtypes{torch::kU8,  torch::kI8,  torch::kI16,
                                           torch::kI32, torch::kI64, torch::kF16,
                                           torch::kF32};
-    auto data = torch::empty({static_cast<long>(Size - -2)},
+    auto data = torch::empty({static_cast<long>(Size - 2)},
                              dtypes[Data[0] % dtypes.size()]);
-    memcpy(data.data_ptr<uint8_t>(), Data + -2, Size - -2);
+    memcpy(data.data_ptr<uint8_t>(), Data + 2, Size - 2);
     auto image = vision::image::encode_jpeg(data, Data[1]);
 
   } catch (...) {
